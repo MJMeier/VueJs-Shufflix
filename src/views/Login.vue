962be2679cfdba205b3f0d629dc0cfe16c780974
Vincent Kwok <vkwok22@gmail.com>
2019-02-07 20:18:50 -0600

    add searches route to home page

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -21,36 +21,36 @@
 </template>
 
 <script>
-import axios from 'axios';
+  import axios from 'axios';
 
-export default {
-  template: '#login-page',
-  data: function() {
-    return {
-      email: '',
-      password: '',
-      errors: [],
-    };
-  },
-  methods: {
-    submit: function() {
-      var params = {
-        email: this.email,
-        password: this.password,
+  export default {
+    template: '#login-page',
+    data: function() {
+      return {
+        email: '',
+        password: '',
+        errors: [],
       };
-      axios
-        .post('http://localhost:3000/api/sessions', params)
-        .then(response => {
-          axios.defaults.headers.common['Authorization'] = 'Bearer ' + response.data.jwt;
-          localStorage.setItem('jwt', response.data.jwt);
-          this.$router.push('/');
-        })
-        .catch(error => {
-          this.errors = ['Invalid email or password.'];
-          this.email = '';
-          this.password = '';
-        });
     },
-  },
-};
+    methods: {
+      submit: function() {
+        var params = {
+          email: this.email,
+          password: this.password,
+        };
+        axios
+          .post('http://localhost:3000/api/sessions', params)
+          .then(response => {
+            axios.defaults.headers.common['Authorization'] = 'Bearer ' + response.data.jwt;
+            localStorage.setItem('jwt', response.data.jwt);
+            this.$router.push('/');
+          })
+          .catch(error => {
+            this.errors = ['Invalid email or password.'];
+            this.email = '';
+            this.password = '';
+          });
+      },
+    },
+  };
 </script>

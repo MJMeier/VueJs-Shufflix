83c94863cc7bc0e41041caf2e3ac225561d7eecd
Michael Meier <mjmeier8@gmail.com>
2019-02-28 18:11:38 -0600

    added some html and css

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,21 @@
 <template>
   <div class="home">
-    <h1>{{ message }}</h1>
-    <router-link to="/signup">Signup </router-link>
+
+        <router-link to="/signup">Signup </router-link>
     <router-link to="/login">Login </router-link>
     <router-link to="/logout">Logout </router-link>
 
+    <div class="jumbotron jumbotron-fluid">
+  <div class="container">
+    <h1 class="display-4">Welcome To Shufflix!</h1>
+    <p class="lead">Please search for a show.</p>
+  </div>
+</div>
+
+
+
+
     <div>
-      <h2>Search for a show:</h2>
       <p>Search: <input type="text" v-model="text" /></p>
       <button v-on:click="search(isError)">Search show</button>
     </div>
@@ -37,20 +46,21 @@
   </div>
 </template>
 
-<style></style>
+<style>
+</style>
 
 <script>
-var axios = require("axios");
+var axios = require('axios');
 
 export default {
   data: function() {
     return {
-      message: "Welcome to our app Shufflix!",
+      message: 'Welcome to our app Shufflix!',
       results: [],
-      text: "",
-      id: "",
+      text: '',
+      id: '',
       episode: [],
-      isError: false
+      isError: false,
     };
   },
 
@@ -58,9 +68,9 @@ export default {
   methods: {
     search: function(isError) {
       var params = {
-        text: this.text
+        text: this.text,
       };
-      axios.post("http://localhost:3000/api/searches", params).then(
+      axios.post('http://localhost:3000/api/searches', params).then(
         function(response) {
           // vince is a bo$$ and came up with this simple AF
           this.results = [];
@@ -71,36 +81,36 @@ export default {
           // console.log(this.results[0].length);
           if (this.results[0].length === 0) {
             this.isError = true;
-            console.log("NO DICE");
+            console.log('NO DICE');
           }
         }.bind(this)
       );
-      console.log("HI:");
+      console.log('HI:');
       // console.log(results);
     },
 
     shuffleSeason: function(thing) {
-      var episode = "";
+      var episode = '';
       var params = {
-        id: thing.id
+        id: thing.id,
       };
-      console.log("ID HERE: ");
+      console.log('ID HERE: ');
       console.log(params);
-      axios.post("http://localhost:3000/api/episodes", params).then(
+      axios.post('http://localhost:3000/api/episodes', params).then(
         function(response) {
           this.episode = [];
           this.episode.push(response.data);
         }.bind(this),
         console.log(this)
       );
-      console.log("BEFORE:");
+      console.log('BEFORE:');
       console.log(thing);
       console.log(thing.visible);
       thing.visible = !thing.visible;
-      console.log("AFTER:");
+      console.log('AFTER:');
       console.log(thing.visible);
-    }
+    },
   },
-  computed: {}
+  computed: {},
 };
 </script>
